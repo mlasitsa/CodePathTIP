f632e1f0a2bbdd4d82501aaa49d4c29f284e03a4
Max <mlasitsa@css.edu>
2025-03-23 12:54:08 -0500

    regular commit

--- Unit 1 (Complete)/Advanced Promlem Set Version 2/Problem8_DefusetheBomb.py
+++ Unit 1 (Complete)/Advanced Promlem Set Version 2/Problem8_DefusetheBomb.py
@@ -7,7 +7,7 @@ To decrypt the code, Batman must replace every number. All the numbers are repla
 If k > 0, replace the ith number with the sum of the next k numbers
 If k < 0, replace the ith number with the sum of the previous k numbers
 If k == 0, replace the ith number with 0.
-As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1].
+As code is circular, the next element of code[n-1] is code[0], and the previous element of code[0] is code[n-1]
 
 Given the circular array code and an integer key k, write a function decrypt() that returns the decrypted code to defuse the bomb!
 
